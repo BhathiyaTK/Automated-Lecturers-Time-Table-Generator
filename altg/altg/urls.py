8317a7a16f86ebaefbbb21988779196ed65056dc
Bhathiya Kariyawasam <bhathiyakariyawasam94@gmail.com>
2020-09-09 17:12:15 +0530

    Old version

--- altg/altg/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('', include('altg_app.urls')),
-    path('admin/', admin.site.urls),
-]
--- altg/altg_app/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class AltgAppConfig(AppConfig):
-    name = 'altg_app'
